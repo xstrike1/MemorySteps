453bdf8cd77a2347cbc18e533691e2722fdec4a6
xstrike1 <skada_xars@yahoo.com>
2021-06-26 15:33:07 +0300

    unhandledException

--- MemoryStepsUI/Program.cs
+++ MemoryStepsUI/Program.cs
@@ -18,7 +18,17 @@ namespace MemoryStepsUI
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += OnUnhandledException;
+
             Application.Run(new MainForm());
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            MessageBox.Show("Unhandled exception: " + ex.Message);
         }
     }
 }
