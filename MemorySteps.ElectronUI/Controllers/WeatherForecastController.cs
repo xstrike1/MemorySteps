8ad1e2c8f1e47bc74b99931187ebe05eebcedb96
xstrike1 <skada_xars@yahoo.com>
2021-07-22 08:16:13 +0300

    net 6 fix

--- MemorySteps.ElectronUI/Controllers/WeatherForecastController.cs
+++ MemorySteps.ElectronUI/Controllers/WeatherForecastController.cs
@@ -26,11 +26,13 @@ namespace MemorySteps.ElectronUI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            Random r = new();
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                TemperatureC = r.Next(-20, 55),
+                Summary = Summaries[r.Next(Summaries.Length)]
             })
             .ToArray();
         }
